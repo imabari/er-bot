f692b908adc6cb90f7e749e015b9c909a9b31269
imabari <32343845+imabari@users.noreply.github.com>
2020-06-10 17:28:16 +0900

    Update er_kouhou.py

--- er_kouhou.py
+++ er_kouhou.py
@@ -24,10 +24,10 @@ if __name__ == "__main__":
     JST = datetime.timezone(datetime.timedelta(hours=+9), "JST")
 
     # 今月
-    # dt_now = datetime.datetime.now(JST)
+    dt_now = datetime.datetime.now(JST)
 
     # 来月
-    dt_now = datetime.datetime.now(JST) + datetime.timedelta(days=28)
+    # dt_now = datetime.datetime.now(JST) + datetime.timedelta(days=28)
 
     # URL作成
     url = f"http://www.city.imabari.ehime.jp/kouhou/koho/{dt_now.year}{dt_now.month:02}/kyukyu.pdf"
