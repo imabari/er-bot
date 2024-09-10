e2e2f0ec55ea89282ed2065fe6f1b9abb4f53bb0
imabari <32343845+imabari@users.noreply.github.com>
2024-09-10 20:31:55 +0900

    Update er_kouhou.py

--- er_kouhou.py
+++ er_kouhou.py
@@ -7,6 +7,7 @@ import fitz
 
 import requests
 import tweepy
+from atproto import Client
 
 
 def download_file(url, save_path):
@@ -84,6 +85,12 @@ def main():
             access_token_secret=access_token_secret,
         )
 
+        at_user = os.environ["AT_USER"]
+        at_pass = os.environ["AT_PASS"]
+
+        api = Client()
+        api.login(at_user, at_pass)
+        
         # PDFを画像に変換
         doc = fitz.open(save_path)
         
@@ -98,6 +105,10 @@ def main():
 
         media = xapi.media_upload(filename=image_path)
         client.create_tweet(text=message, media_ids=[media.media_id])
+
+        with open(image_path, "rb") as f:
+            img_data = f.read()
+            api.send_image(text=text, image=img_data, image_alt=f"{year}年{month}月 救急病院")
 
 if __name__ == "__main__":
     main()
