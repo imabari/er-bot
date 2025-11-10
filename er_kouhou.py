5d76f8c7f2cdb888abc404dc78fb5382d59308d5
imabari <32343845+imabari@users.noreply.github.com>
2025-11-10 20:54:37 +0900

    Update er_kouhou.py

--- er_kouhou.py
+++ er_kouhou.py
@@ -101,7 +101,7 @@ def main():
         # X
 
         image_path = "kyukyu1.png"
-        message = f"{month}月の救急病院などの当直表 #imabari\n{url}\n\nえひめ医療情報ネット\nhttps://www.iryou.teikyouseido.mhlw.go.jp/znk-web/juminkanja/S2310/initialize?pref=38\n\n本日の当番医\nhttps://imabari119.github.io/today/"
+        message = f"{month}月の救急病院などの当直表 #imabari\n{url}\n\nえひめ医療情報ネット\nhttps://www.iryou.teikyouseido.mhlw.go.jp/znk-web/juminkanja/S2310/initialize?pref=38\n\n今治市の医療情報\nhttps://imabari.jpn.org/imabari119/"
 
         media = xapi.media_upload(filename=image_path)
         client.create_tweet(text=message, media_ids=[media.media_id])
